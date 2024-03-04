13aca9cb453d1d5eaf3d8daacb6909b8cc85f19f
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2024-03-04 09:19:22 +0530

    ota-json: Update DroidX Version Flag

--- generate_json.sh
+++ generate_json.sh
@@ -7,7 +7,7 @@ maintainer="Name (nickname)"
 oem="xiaomi"
 device="lancelot" 
 devicename="Redmi 9" 
-droidx="2.0"
+droidx_version="2.0"
 zip="droidx-2.0-20240225-1116-OFFICIAL-lancelot-Gapps.zip"
 forum=""
 gapps=""
@@ -34,7 +34,7 @@ echo '{
         "maintainer": "'$maintainer'",
         "oem": "'$oem'",
         "device": "'$devicename'",
-        "droidx": "'$droidx'",
+        "droidx-version": "'$droidx_version'",
         "filename": "'$zip_only'",
         "download": "https://sourceforge.net/projects/droidxui-releases/files/'$device'/'$v_max'.x/'$zip_only'/download",
         "timestamp": '$timestamp',
